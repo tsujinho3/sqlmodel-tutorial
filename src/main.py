e7aa0ad5797727128356fc7fc0c0f3d681831676
tsujinho3 <29732798+tsujinho3@users.noreply.github.com>
2023-04-27 00:06:06 +0900

    feat: read data with where method

--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
-from sqlmodel import Field, Session, SQLModel, create_engine, select
+from typing import Optional
+
+from sqlmodel import Field, Session, SQLModel, col, create_engine, select
 
 
 class Hero(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
+    id: Optional[int] = Field(default=None, primary_key=True)
     name: str
     secret_name: str
-    age: int | None = None
+    age: Optional[int] = None
 
 
 sqlite_file_name = "database.db"
@@ -22,24 +24,34 @@ def create_heroes() -> None:
     hero_1 = Hero(name="Deadpond", secret_name="Dive Wilson")
     hero_2 = Hero(name="Spider-Boy", secret_name="Pedro Parqueador")
     hero_3 = Hero(name="Rusty-Man", secret_name="Tommy Sharp", age=48)
+    hero_4 = Hero(name="Tarantula", secret_name="Natalia Roman-on", age=32)
+    hero_5 = Hero(name="Black Lion", secret_name="Trevor Challa", age=35)
+    hero_6 = Hero(name="Dr. Weird", secret_name="Steve Weird", age=36)
+    hero_7 = Hero(name="Captain North America", secret_name="Esteban Rogelios", age=93)
 
     with Session(engine) as session:
         session.add(hero_1)
         session.add(hero_2)
         session.add(hero_3)
+        session.add(hero_4)
+        session.add(hero_5)
+        session.add(hero_6)
+        session.add(hero_7)
 
         session.commit()
 
 
 def select_heroes() -> None:
     with Session(engine) as session:
-        heroes = session.exec(select(Hero)).all()
-        print(heroes)
+        statement = select(Hero).where(col(Hero.age) >= 35)
+        results = session.exec(statement)
+        for hero in results:
+            print(hero)
 
 
 def main() -> None:
-    # create_db_and_tables()
-    # create_heroes()
+    create_db_and_tables()
+    create_heroes()
     select_heroes()
 
 
