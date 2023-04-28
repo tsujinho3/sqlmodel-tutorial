39e32423afe113dbc5f7b04cc278d014676139d5
tsujinho3 <29732798+tsujinho3@users.noreply.github.com>
2023-04-28 22:52:09 +0900

    feat: update data

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
-from pprint import pprint as print
 from typing import Optional
 
-from sqlmodel import Field, Session, SQLModel, col, create_engine, select
+from sqlmodel import Field, Session, SQLModel, create_engine, select
 
 
 class Hero(SQLModel, table=True):
@@ -42,18 +41,38 @@ def create_heroes() -> None:
         session.commit()
 
 
-def select_heroes() -> None:
+def update_heroes() -> None:
     with Session(engine) as session:
-        statement = select(Hero).where(col(Hero.age) > 32).limit(3)
+        statement = select(Hero).where(Hero.name == "Spider-Boy")
         results = session.exec(statement)
-        heroes = results.all()
-        print(heroes)
+        hero_1 = results.one()
+        print("Hero 1:", hero_1)
+
+        statement = select(Hero).where(Hero.name == "Captain North America")
+        results = session.exec(statement)
+        hero_2 = results.one()
+        print("Hero 2:", hero_2)
+
+        hero_1.age = 16
+        hero_1.name = "Spider-Youngster"
+        session.add(hero_1)
+
+        hero_2.name = "Captain North America Except Canada"  #
+        hero_2.age = 110
+        session.add(hero_2)
+
+        session.commit()
+        session.refresh(hero_1)
+        session.refresh(hero_2)
+
+        print("Updated hero 1:", hero_1)
+        print("Updated hero 2:", hero_2)
 
 
 def main() -> None:
     create_db_and_tables()
     create_heroes()
-    select_heroes()
+    update_heroes()
 
 
 if __name__ == "__main__":
