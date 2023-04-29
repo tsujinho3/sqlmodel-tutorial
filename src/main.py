4a403344ef36495b4db8c84770921d7ede9f67aa
tsujinho3 <29732798+tsujinho3@users.noreply.github.com>
2023-04-29 13:54:40 +0900

    feat: delete data

--- src/main.py
+++ src/main.py
@@ -69,10 +69,31 @@ def update_heroes() -> None:
         print("Updated hero 2:", hero_2)
 
 
+def delete_heroes() -> None:
+    with Session(engine) as session:
+        statement = select(Hero).where(Hero.name == "Spider-Youngster")
+        results = session.exec(statement)
+        hero: Hero | None = results.one()
+        print("Hero: ", hero)
+
+        session.delete(hero)
+        session.commit()
+
+        print("Deleted hero:", hero)
+
+        statement = select(Hero).where(Hero.name == "Spider-Youngster")
+        results = session.exec(statement)
+        hero = results.first()
+
+        if hero is None:
+            print("There's no hero named Spider-Youngster")
+
+
 def main() -> None:
     create_db_and_tables()
     create_heroes()
     update_heroes()
+    delete_heroes()
 
 
 if __name__ == "__main__":
