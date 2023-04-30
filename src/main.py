e8c71f20d5884deb0e55be34e877f270dc9b404d
tsujinho3 <29732798+tsujinho3@users.noreply.github.com>
2023-04-30 12:21:37 +0900

    feat: create relationship data

--- src/main.py
+++ src/main.py
@@ -33,8 +33,45 @@ def create_db_and_tables() -> None:
     SQLModel.metadata.create_all(engine)
 
 
+def create_heroes() -> None:
+    with Session(engine) as session:
+        team_preventers = Team(name="Preventers", headquarters="Sharp Tower")
+        team_z_force = Team(name="Z-Force", headquarters="Sister Margaret’s Bar")
+
+        hero_deadpond = Hero(
+            name="Deadpond",
+            secret_name="Dive Wilson",
+            teams=[team_z_force, team_preventers],
+        )
+        hero_rusty_man = Hero(
+            name="Rusty-Man",
+            secret_name="Tommy Sharp",
+            age=48,
+            teams=[team_preventers],
+        )
+        hero_spider_boy = Hero(
+            name="Spider-Boy", secret_name="Pedro Parqueador", teams=[team_preventers]
+        )
+        session.add(hero_deadpond)
+        session.add(hero_rusty_man)
+        session.add(hero_spider_boy)
+        session.commit()
+
+        session.refresh(hero_deadpond)
+        session.refresh(hero_rusty_man)
+        session.refresh(hero_spider_boy)
+
+        print("Deadpond:", hero_deadpond)
+        print("Deadpond teams:", hero_deadpond.teams)
+        print("Rusty-Man:", hero_rusty_man)
+        print("Rusty-Man Teams:", hero_rusty_man.teams)
+        print("Spider-Boy:", hero_spider_boy)
+        print("Spider-Boy Teams:", hero_spider_boy.teams)
+
+
 def main() -> None:
     create_db_and_tables()
+    create_heroes()
 
 
 if __name__ == "__main__":
