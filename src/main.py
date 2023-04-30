02a409dac4a167311b7ef086356ef61e7dc1ae02
tsujinho3 <29732798+tsujinho3@users.noreply.github.com>
2023-04-30 13:45:39 +0900

    feat: update and remove many-to-many relationship

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from sqlmodel import Field, Relationship, Session, SQLModel, create_engine
+from sqlmodel import Field, Relationship, Session, SQLModel, create_engine, select
 
 
 class HeroTeamLink(SQLModel, table=True):
@@ -69,9 +69,32 @@ def create_heroes() -> None:
         print("Spider-Boy Teams:", hero_spider_boy.teams)
 
 
+def update_heroes() -> None:
+    with Session(engine) as session:
+        hero_spider_boy = session.exec(
+            select(Hero).where(Hero.name == "Spider-Boy")
+        ).one()
+        team_z_force = session.exec(select(Team).where(Team.name == "Z-Force")).one()
+
+        team_z_force.heroes.append(hero_spider_boy)
+        session.add(team_z_force)
+        session.commit()
+
+        print("Updated Spider-Boy's Teams:", hero_spider_boy.teams)
+        print("Z-Force heroes:", team_z_force.heroes)
+
+        hero_spider_boy.teams.remove(team_z_force)
+        session.add(team_z_force)
+        session.commit()
+
+        print("Reverted Z-Force's heroes:", team_z_force.heroes)
+        print("Reverted Spider-Boy's teams:", hero_spider_boy.teams)
+
+
 def main() -> None:
     create_db_and_tables()
     create_heroes()
+    update_heroes()
 
 
 if __name__ == "__main__":
