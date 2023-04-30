2813c142818b149841be57ec583e66694a1fc6e1
tsujinho3 <29732798+tsujinho3@users.noreply.github.com>
2023-04-30 11:04:12 +0900

    feat: create many-to-many relationship

--- src/main.py
+++ src/main.py
@@ -1,19 +1,9 @@
-from sqlmodel import Field, Relationship, SQLModel, create_engine
+from sqlmodel import Field, Relationship, Session, SQLModel, create_engine
 
 
-class Weapon(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    name: str = Field(index=True)
-
-    hero: "Hero" = Relationship(back_populates="weapon")
-
-
-class Power(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    name: str = Field(index=True)
-
-    hero_id: int = Field(foreign_key="hero.id")
-    hero: "Hero" = Relationship(back_populates="powers")
+class HeroTeamLink(SQLModel, table=True):
+    team_id: int | None = Field(default=None, foreign_key="team.id", primary_key=True)
+    hero_id: int | None = Field(default=None, foreign_key="hero.id", primary_key=True)
 
 
 class Team(SQLModel, table=True):
@@ -21,7 +11,7 @@ class Team(SQLModel, table=True):
     name: str = Field(index=True)
     headquarters: str
 
-    heroes: list["Hero"] = Relationship(back_populates="team")
+    heroes: list["Hero"] = Relationship(back_populates="teams", link_model=HeroTeamLink)
 
 
 class Hero(SQLModel, table=True):
@@ -30,13 +20,7 @@ class Hero(SQLModel, table=True):
     secret_name: str
     age: int | None = Field(default=None, index=True)
 
-    team_id: int | None = Field(default=None, foreign_key="team.id")
-    team: Team | Team = Relationship(back_populates="heroes")
-
-    weapon_id: int | None = Field(default=None, foreign_key="weapon.id")
-    weapon: Weapon | None = Relationship(back_populates="hero")
-
-    powers: list[Power] = Relationship(back_populates="hero")
+    teams: list[Team] = Relationship(back_populates="heroes", link_model=HeroTeamLink)
 
 
 sqlite_file_name = "database.db"
